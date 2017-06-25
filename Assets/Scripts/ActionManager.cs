926d2bdf661a334507229fbaea14ea79d2aebe80
Jonathan Granskog <jonathan.granskog@gmail.com>
2017-06-25 22:29:06 +0300

    Created action manager and simple trigger pointer

---
+++ Assets/Scripts/ActionManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour {
+
+    public GameObject pageManagerObject;
+
+    private PageManager pageManager;
+    private bool menuOpen = false;
+    private bool triggerHeld = false;
+    private GameObject cursor;
+    private MeshRenderer cursorRenderer;
+
+    void Start()
+    {
+        pageManager = pageManagerObject.GetComponent<PageManager>();
+        cursor = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        cursorRenderer = cursor.GetComponent<MeshRenderer>();
+        cursor.transform.localScale = 0.05f * Vector3.one;
+        cursorRenderer.enabled = false;
+        Destroy(cursor.GetComponent<SphereCollider>());
+    }
+
+    private void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (triggerHeld)
+            {
+                cursorRenderer.enabled = true;
+                cursor.transform.position = hit.point;
+            }
+        } else if (triggerHeld)
+        {
+            cursorRenderer.enabled = false;
+        }
+    }
+
+    public void TriggerPress()
+    {
+        // TODO: Create laser pointer if it does not hit menu
+        Debug.Log("Trigger pressed!");
+        triggerHeld = true;
+        cursorRenderer.enabled = true;
+    }
+
+    public void TriggerUnpress()
+    {
+        triggerHeld = false;
+        cursorRenderer.enabled = false;
+    }
+
+    public void PadRightClick()
+    {
+        pageManager.NextPage();
+    }
+
+    public void PadLeftClick()
+    {
+        pageManager.PreviousPage();
+    }
+
+    public void MenuClick()
+    {
+        // TODO: Open or close menu
+        Debug.Log("Menu button clicked!");
+    }
+}
---
+++ Assets/Scripts/SteamVRControllerActions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamVRControllerActions : MonoBehaviour {
+
+    private ActionManager actionManager;
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_TrackedController controller;
+
+    private void Start()
+    {
+        actionManager = GetComponent<ActionManager>();
+    }
+
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void OnEnable()
+    {
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.PadClicked += PadClick;
+        controller.TriggerClicked += TriggerClick;
+        controller.TriggerUnclicked += TriggerUnclick;
+        controller.MenuButtonClicked += MenuClick;
+    }
+
+    private void OnDisable()
+    {
+        controller.PadClicked -= PadClick;
+    }
+
+    private void PadClick(object sender, ClickedEventArgs e)
+    {
+        if (e.padX < 0)
+        {
+            actionManager.PadLeftClick();
+        } else
+        {
+            actionManager.PadRightClick();
+        }
+    }
+
+    private void TriggerClick(object sender, ClickedEventArgs e)
+    {
+        actionManager.TriggerPress();
+    }
+
+    private void TriggerUnclick(object sender, ClickedEventArgs e)
+    {
+        actionManager.TriggerUnpress();
+    }
+
+    private void MenuClick(object sender, ClickedEventArgs e)
+    {
+        actionManager.MenuClick();
+    }
+
+}
