6aa229747ca440a22d9cd59477e2f466de9c3665
Jonathan Granskog <jonathan.granskog@gmail.com>
2017-07-15 22:12:58 +0300

    Made ActionManager an abstract class and SceneActionManager replaces old ActionManager

--- Assets/Scripts/ActionManager.cs
+++ Assets/Scripts/ActionManager.cs
@@ -1,126 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
-public class ActionManager : MonoBehaviour {
+abstract public class ActionManager : MonoBehaviour {
 
-    private enum HitObject
-    {
-        None,
-        Menu,
-        Timer,
-        Screen
-    }
-
-    public GameObject pageManagerObject;
     public Material cursorMaterial;
-    public float rayCastMaxDist = 20.0f;
-    public GameObject menuObject;
-    public GameObject timerObject;
+
+    protected GameObject cursor;
+    protected MeshRenderer cursorRenderer;
+    protected int cursorRaycastMask;
     
-    private PageManager pageManager;
-    private bool triggerHeld = false;
-    private GameObject cursor;
-    private MeshRenderer cursorRenderer;
-    private int cursorRaycastMask;
-    private MenuManager menu;
-    private TimerCounter timer;
-    private Ray ray;
-    private HitObject hitObj;
-    
-    void Start()
-    {
-        pageManager = pageManagerObject.GetComponent<PageManager>();
-        menu = menuObject.GetComponent<MenuManager>();
-        timer = timerObject.GetComponent<TimerCounter>();
-        CreateCursor();
-        int screenLayer = 1 << 31;
-        cursorRaycastMask = screenLayer;
-    }
+    abstract public void TriggerPress();
 
-    private void Update()
-    {
-        ray = new Ray(transform.position, transform.forward);
-        cursorRenderer.enabled = false;
-        hitObj = HitObject.None;
+    abstract public void TriggerUnpress();
 
-        RaycastHit menuHit = new RaycastHit(); bool hitMenu;
-        if (hitMenu = menu.Raycast(ray, out menuHit))
-        {
-            cursorRenderer.enabled = true;
-            cursor.transform.position = menuHit.point;
-            cursor.transform.forward = menuHit.normal;
-            hitObj = HitObject.Menu;
-        }
+    abstract public void PadRightClick();
 
-        RaycastHit timerHit = new RaycastHit(); bool hitTimer;
-        if (hitTimer = timer.Raycast(ray, out timerHit))
-        {
-            if (!hitMenu || timerHit.distance < menuHit.distance)
-            {
-                cursorRenderer.enabled = true;
-                cursor.transform.position = timerHit.point;
-                cursor.transform.forward = timerHit.normal;
-                hitObj = HitObject.Timer;
-            }
-        }
+    abstract public void PadLeftClick();
 
-        if (triggerHeld)
-        {
-            RaycastHit screenHit = new RaycastHit();
-            if (Physics.Raycast(ray, out screenHit, rayCastMaxDist, cursorRaycastMask))
-            {
-                if ((!hitMenu || screenHit.distance < menuHit.distance) &&
-                    (!hitTimer || screenHit.distance < timerHit.distance))
-                {
-                    cursorRenderer.enabled = true;
-                    cursor.transform.position = screenHit.point;
-                    cursor.transform.forward = screenHit.normal;
-                    hitObj = HitObject.Screen;
-                }   
-            }
-        }
-
-    }
-
-    public void TriggerPress()
-    {
-        if (hitObj == HitObject.Menu)
-        {
-            menu.TriggerAction(ray);
-        } else if (hitObj == HitObject.Timer)
-        {
-            timer.TriggerAction(ray);
-        }
-
-        triggerHeld = true;
-    }
-
-    public void TriggerUnpress()
-    {
-        triggerHeld = false;
-    }
-
-    public void PadRightClick()
-    {
-        if (menu.isOpen()) menu.NextPage();
-        else if (hitObj == HitObject.Timer) timer.RightPadStep();
-        else pageManager.NextPage();
-    }
-
-    public void PadLeftClick()
-    {
-        if (menu.isOpen()) menu.PreviousPage();
-        else if (hitObj == HitObject.Timer) timer.LeftPadStep();
-        else pageManager.PreviousPage();
-    }
-
-    public void MenuClick()
-    {
-        if (pageManager.loading) pageManager.Interrupt();
-        else menu.Toggle();
-    }
+    abstract public void MenuClick();
 
     private void CreateCursor()
     {
---
+++ Assets/Scripts/SceneActionManager.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+
+public class SceneActionManager : ActionManager {
+
+    private enum HitObject
+    {
+        None,
+        Menu,
+        Timer,
+        Screen
+    }
+
+    public GameObject pageManagerObject;
+    public float rayCastMaxDist = 20.0f;
+    public GameObject menuObject;
+    public GameObject timerObject;
+
+    private PageManager pageManager;
+    private bool triggerHeld = false;
+    private MenuManager menu;
+    private TimerCounter timer;
+    private Ray ray;
+    private HitObject hitObj;
+
+    void Start()
+    {
+        pageManager = pageManagerObject.GetComponent<PageManager>();
+        menu = menuObject.GetComponent<MenuManager>();
+        timer = timerObject.GetComponent<TimerCounter>();
+        CreateCursor();
+        int screenLayer = 1 << 31;
+        cursorRaycastMask = screenLayer;
+    }
+
+    private void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+        cursorRenderer.enabled = false;
+        hitObj = HitObject.None;
+
+        RaycastHit menuHit = new RaycastHit(); bool hitMenu;
+        if (hitMenu = menu.Raycast(ray, out menuHit))
+        {
+            cursorRenderer.enabled = true;
+            cursor.transform.position = menuHit.point;
+            cursor.transform.forward = menuHit.normal;
+            hitObj = HitObject.Menu;
+        }
+
+        RaycastHit timerHit = new RaycastHit(); bool hitTimer;
+        if (hitTimer = timer.Raycast(ray, out timerHit))
+        {
+            if (!hitMenu || timerHit.distance < menuHit.distance)
+            {
+                cursorRenderer.enabled = true;
+                cursor.transform.position = timerHit.point;
+                cursor.transform.forward = timerHit.normal;
+                hitObj = HitObject.Timer;
+            }
+        }
+
+        if (triggerHeld)
+        {
+            RaycastHit screenHit = new RaycastHit();
+            if (Physics.Raycast(ray, out screenHit, rayCastMaxDist, cursorRaycastMask))
+            {
+                if ((!hitMenu || screenHit.distance < menuHit.distance) &&
+                    (!hitTimer || screenHit.distance < timerHit.distance))
+                {
+                    cursorRenderer.enabled = true;
+                    cursor.transform.position = screenHit.point;
+                    cursor.transform.forward = screenHit.normal;
+                    hitObj = HitObject.Screen;
+                }
+            }
+        }
+
+    }
+
+    override public void TriggerPress()
+    {
+        if (hitObj == HitObject.Menu)
+        {
+            menu.TriggerAction(ray);
+        }
+        else if (hitObj == HitObject.Timer)
+        {
+            timer.TriggerAction(ray);
+        }
+
+        triggerHeld = true;
+    }
+
+    override public void TriggerUnpress()
+    {
+        triggerHeld = false;
+    }
+
+    override public void PadRightClick()
+    {
+        if (menu.isOpen()) menu.NextPage();
+        else if (hitObj == HitObject.Timer) timer.RightPadStep();
+        else pageManager.NextPage();
+    }
+
+    override public void PadLeftClick()
+    {
+        if (menu.isOpen()) menu.PreviousPage();
+        else if (hitObj == HitObject.Timer) timer.LeftPadStep();
+        else pageManager.PreviousPage();
+    }
+
+    override public void MenuClick()
+    {
+        if (pageManager.loading) pageManager.Interrupt();
+        else menu.Toggle();
+    }
+
+    private void CreateCursor()
+    {
+        GameObject cursorObj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        cursorRenderer = cursorObj.GetComponent<MeshRenderer>();
+        cursorObj.transform.localScale = 0.025f * (new Vector3(1, 0.01f, 1));
+        cursorRenderer.enabled = false;
+        cursorRenderer.material = cursorMaterial;
+        cursorObj.transform.Rotate(90, 0, 0);
+        Destroy(cursorObj.GetComponent<CapsuleCollider>());
+        cursor = new GameObject();
+        cursor.name = "Cursor";
+        cursorObj.transform.parent = cursor.transform;
+    }
+}
